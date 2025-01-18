935b7579fe073b8cdbe6cac7411fe363160c93ce
Mitul Patel <me@mitulpa.tel>
2025-01-18 10:15:43 -0500

    feat(config): add SOURCEBOOKS data for the cli

---
+++ src/config/sourcebooks.js
@@ -0,0 +1,74 @@
+export const SOURCEBOOKS = {
+    PHB: {
+        name: "Player's Handbook",
+        abbreviation: "PHB",
+        enabled: true,
+        core: true
+    },
+    DMG: {
+        name: "Dungeon Master's Guide",
+        abbreviation: "DMG",
+        enabled: true,
+        core: true
+    },
+    MM: {
+        name: "Monster Manual",
+        abbreviation: "MM",
+        enabled: true,
+        core: true
+    },
+    SCAG: {
+        name: "Sword Coast Adventurer's Guide",
+        abbreviation: "SCAG",
+        enabled: true,
+        core: false
+    },
+    VGM: {
+        name: "Volo's Guide to Monsters",
+        abbreviation: "VGM",
+        enabled: true,
+        core: false
+    },
+    XGE: {
+        name: "Xanathar's Guide to Everything",
+        abbreviation: "XGE",
+        enabled: true,
+        core: false
+    },
+    MTF: {
+        name: "Mordenkainen's Tome of Foes",
+        abbreviation: "MTF",
+        enabled: true,
+        core: false
+    },
+    ERLW: {
+        name: "Eberron: Rising from the Last War",
+        abbreviation: "ERLW",
+        enabled: true,
+        core: false
+    },
+    TCE: {
+        name: "Tasha's Cauldron of Everything",
+        abbreviation: "TCE",
+        enabled: true,
+        core: false
+    },
+    VRGR: {
+        name: "Van Richten's Guide to Ravenloft",
+        abbreviation: "VRGR",
+        enabled: true,
+        core: false
+    },
+    FTD: {
+        name: "Fizban's Treasury of Dragons",
+        abbreviation: "FTD",
+        enabled: true,
+        core: false
+    },
+    SCC: {
+        name: "Strixhaven: A Curriculum of Chaos",
+        abbreviation: "SCC",
+        enabled: true,
+        core: false
+    }
+};
