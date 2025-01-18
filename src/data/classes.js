19187373b8d9dd6721e2b0cfbf4df9617ee0aaa0
Mitul Patel <me@mitulpa.tel>
2025-01-18 10:16:15 -0500

    feat(data): add class & race data for the cli

---
+++ src/data/classes.js
@@ -0,0 +1,204 @@
+export const CLASSES = {
+    // PHB Classes
+    BARBARIAN: {
+        name: "Barbarian",
+        source: "PHB",
+        subclasses: {
+            "Path of the Berserker": "PHB",
+            "Path of the Totem Warrior": "PHB",
+            "Path of the Ancestral Guardian": "XGE",
+            "Path of the Storm Herald": "XGE",
+            "Path of the Zealot": "XGE",
+            "Path of the Beast": "TCE",
+            "Path of Wild Magic": "TCE"
+        },
+        hasOptionalFeatures: true
+    },
+    BARD: {
+        name: "Bard",
+        source: "PHB",
+        subclasses: {
+            "College of Lore": "PHB",
+            "College of Valor": "PHB",
+            "College of Glamour": "XGE",
+            "College of Swords": "XGE",
+            "College of Whispers": "XGE",
+            "College of Creation": "TCE",
+            "College of Eloquence": "TCE"
+        },
+        hasOptionalFeatures: true
+    },
+    CLERIC: {
+        name: "Cleric",
+        source: "PHB",
+        subclasses: {
+            "Knowledge Domain": "PHB",
+            "Life Domain": "PHB",
+            "Light Domain": "PHB",
+            "Nature Domain": "PHB",
+            "Tempest Domain": "PHB",
+            "Trickery Domain": "PHB",
+            "War Domain": "PHB",
+            "Death Domain": "DMG",
+            "Arcana Domain": "SCAG",
+            "Forge Domain": "XGE",
+            "Grave Domain": "XGE",
+            "Order Domain": "TCE",
+            "Peace Domain": "TCE",
+            "Twilight Domain": "TCE"
+        },
+        hasOptionalFeatures: true
+    },
+    DRUID: {
+        name: "Druid",
+        source: "PHB",
+        subclasses: {
+            "Circle of the Land": "PHB",
+            "Circle of the Moon": "PHB",
+            "Circle of Dreams": "XGE",
+            "Circle of the Shepherd": "XGE",
+            "Circle of Spores": "TCE",
+            "Circle of Stars": "TCE",
+            "Circle of Wildfire": "TCE"
+        },
+        hasOptionalFeatures: true
+    },
+    FIGHTER: {
+        name: "Fighter",
+        source: "PHB",
+        subclasses: {
+            "Champion": "PHB",
+            "Battle Master": "PHB",
+            "Eldritch Knight": "PHB",
+            "Purple Dragon Knight": "SCAG",
+            "Arcane Archer": "XGE",
+            "Cavalier": "XGE",
+            "Samurai": "XGE",
+            "Echo Knight": "ERLW",
+            "Psi Warrior": "TCE",
+            "Rune Knight": "TCE"
+        },
+        hasOptionalFeatures: true
+    },
+    MONK: {
+        name: "Monk",
+        source: "PHB",
+        subclasses: {
+            "Way of the Open Hand": "PHB",
+            "Way of Shadow": "PHB",
+            "Way of the Four Elements": "PHB",
+            "Way of the Sun Soul": "SCAG/XGE",
+            "Way of the Drunken Master": "XGE",
+            "Way of the Kensei": "XGE",
+            "Way of Mercy": "TCE",
+            "Way of the Astral Self": "TCE"
+        },
+        hasOptionalFeatures: true
+    },
+    PALADIN: {
+        name: "Paladin",
+        source: "PHB",
+        subclasses: {
+            "Oath of Devotion": "PHB",
+            "Oath of the Ancients": "PHB",
+            "Oath of Vengeance": "PHB",
+            "Oath of the Crown": "SCAG",
+            "Oath of Conquest": "XGE",
+            "Oath of Redemption": "XGE",
+            "Oath of Glory": "TCE",
+            "Oath of the Watchers": "TCE"
+        },
+        hasOptionalFeatures: true
+    },
+    RANGER: {
+        name: "Ranger",
+        source: "PHB",
+        subclasses: {
+            "Hunter": "PHB",
+            "Beast Master": "PHB",
+            "Gloom Stalker": "XGE",
+            "Horizon Walker": "XGE",
+            "Monster Slayer": "XGE",
+            "Fey Wanderer": "TCE",
+            "Swarmkeeper": "TCE",
+            "Drakewarden": "FTD"
+        },
+        hasOptionalFeatures: true
+    },
+    ROGUE: {
+        name: "Rogue",
+        source: "PHB",
+        subclasses: {
+            "Thief": "PHB",
+            "Assassin": "PHB",
+            "Arcane Trickster": "PHB",
+            "Mastermind": "SCAG/XGE",
+            "Swashbuckler": "SCAG/XGE",
+            "Inquisitive": "XGE",
+            "Scout": "XGE",
+            "Phantom": "TCE",
+            "Soulknife": "TCE"
+        },
+        hasOptionalFeatures: true
+    },
+    SORCERER: {
+        name: "Sorcerer",
+        source: "PHB",
+        subclasses: {
+            "Draconic Bloodline": "PHB",
+            "Wild Magic": "PHB",
+            "Divine Soul": "XGE",
+            "Shadow Magic": "XGE",
+            "Storm Sorcery": "XGE",
+            "Aberrant Mind": "TCE",
+            "Clockwork Soul": "TCE"
+        },
+        hasOptionalFeatures: true
+    },
+    WARLOCK: {
+        name: "Warlock",
+        source: "PHB",
+        subclasses: {
+            "The Archfey": "PHB",
+            "The Fiend": "PHB",
+            "The Great Old One": "PHB",
+            "The Undying": "SCAG",
+            "The Celestial": "XGE",
+            "The Hexblade": "XGE",
+            "The Genie": "TCE",
+            "The Fathomless": "TCE",
+            "The Undead": "VRGR"
+        },
+        hasOptionalFeatures: true
+    },
+    WIZARD: {
+        name: "Wizard",
+        source: "PHB",
+        subclasses: {
+            "School of Abjuration": "PHB",
+            "School of Conjuration": "PHB",
+            "School of Divination": "PHB",
+            "School of Enchantment": "PHB",
+            "School of Evocation": "PHB",
+            "School of Illusion": "PHB",
+            "School of Necromancy": "PHB",
+            "School of Transmutation": "PHB",
+            "War Magic": "XGE",
+            "Bladesinging": "TCE",
+            "Order of Scribes": "TCE"
+        },
+        hasOptionalFeatures: true
+    },
+    // Artificer (Eberron/Tasha's)
+    ARTIFICER: {
+        name: "Artificer",
+        source: "TCE",
+        subclasses: {
+            "Alchemist": "TCE",
+            "Armorer": "TCE",
+            "Artillerist": "TCE",
+            "Battle Smith": "TCE"
+        },
+        hasOptionalFeatures: true
+    }
+};
---
+++ src/data/races.js
@@ -0,0 +1,137 @@
+export const RACES = {
+    // PHB Races
+    DRAGONBORN: {
+        name: "Dragonborn",
+        source: "PHB",
+        subraces: [],
+        hasVariant: true
+    },
+    DWARF: {
+        name: "Dwarf",
+        source: "PHB",
+        subraces: ["Hill Dwarf", "Mountain Dwarf"],
+        hasVariant: true
+    },
+    ELF: {
+        name: "Elf",
+        source: "PHB",
+        subraces: ["High Elf", "Wood Elf", "Dark Elf"],
+        hasVariant: true
+    },
+    GNOME: {
+        name: "Gnome",
+        source: "PHB",
+        subraces: ["Forest Gnome", "Rock Gnome"],
+        hasVariant: true
+    },
+    HALF_ELF: {
+        name: "Half-Elf",
+        source: "PHB",
+        subraces: [],
+        hasVariant: true
+    },
+    HALFLING: {
+        name: "Halfling",
+        source: "PHB",
+        subraces: ["Lightfoot Halfling", "Stout Halfling"],
+        hasVariant: true
+    },
+    HALF_ORC: {
+        name: "Half-Orc",
+        source: "PHB",
+        subraces: [],
+        hasVariant: true
+    },
+    HUMAN: {
+        name: "Human",
+        source: "PHB",
+        subraces: [],
+        hasVariant: true
+    },
+    TIEFLING: {
+        name: "Tiefling",
+        source: "PHB",
+        subraces: [],
+        hasVariant: true
+    },
+
+    // Volo's Guide Races
+    AASIMAR: {
+        name: "Aasimar",
+        source: "VGM",
+        subraces: ["Protector Aasimar", "Scourge Aasimar", "Fallen Aasimar"],
+        hasVariant: true
+    },
+    FIRBOLG: {
+        name: "Firbolg",
+        source: "VGM",
+        subraces: [],
+        hasVariant: true
+    },
+    GOLIATH: {
+        name: "Goliath",
+        source: "VGM",
+        subraces: [],
+        hasVariant: true
+    },
+    KENKU: {
+        name: "Kenku",
+        source: "VGM",
+        subraces: [],
+        hasVariant: true
+    },
+    LIZARDFOLK: {
+        name: "Lizardfolk",
+        source: "VGM",
+        subraces: [],
+        hasVariant: true
+    },
+    TABAXI: {
+        name: "Tabaxi",
+        source: "VGM",
+        subraces: [],
+        hasVariant: true
+    },
+    TRITON: {
+        name: "Triton",
+        source: "VGM",
+        subraces: [],
+        hasVariant: true
+    },
+
+    // Mordenkainen's Tome of Foes Races
+    GITH: {
+        name: "Gith",
+        source: "MTF",
+        subraces: ["Githyanki", "Githzerai"],
+        hasVariant: true
+    },
+
+    // Eberron Races
+    WARFORGED: {
+        name: "Warforged",
+        source: "ERLW",
+        subraces: [],
+        hasVariant: true
+    },
+    CHANGELING: {
+        name: "Changeling",
+        source: "ERLW",
+        subraces: [],
+        hasVariant: true
+    },
+    SHIFTER: {
+        name: "Shifter",
+        source: "ERLW",
+        subraces: ["Beasthide", "Longtooth", "Swiftstride", "Wildhunt"],
+        hasVariant: true
+    },
+
+    // Custom Lineage (Tasha's)
+    CUSTOM: {
+        name: "Custom Lineage",
+        source: "TCE",
+        subraces: [],
+        hasVariant: false
+    }
+};
